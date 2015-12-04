11d05c5bb5583ef488f96621fabfee5dcf77770a
Leonie Reichert <leonie.reichert@stud-mail.uni-wuerzburg.de>
2015-12-04 19:10:15 +0100

    sortierung funst

--- src/catalog.cpp
+++ src/catalog.cpp
@@ -21,7 +21,10 @@ public:
 		double mag;
 		double azimut;
 		double elevation;
+		
+		StarCatalogEntry();
 		StarCatalogEntry(StarCatalogEntry*, int, double, double, double, double, double, double);
+
 		void setNext(StarCatalogEntry*);
 		StarCatalogEntry* getNext();
 };
@@ -36,11 +39,23 @@ StarCatalogEntry::StarCatalogEntry(StarCatalogEntry *before,
 		this -> mag=mag;
 		this -> azimut=azimut;
 		this-> elevation=elevation;
+		this->next=NULL;
 }
+
+StarCatalogEntry::StarCatalogEntry(){
+
+	this->next=NULL;
+	this->id=-88;
+	this->before=NULL;
+}
+
 void StarCatalogEntry::setNext(StarCatalogEntry *next){
 		this->next=next;
 
 }
+
+
+
 
 StarCatalogEntry* StarCatalogEntry::getNext(){
 		return this->next;
@@ -58,13 +73,20 @@ public:
 	double beta;
 	double alpha1;
 	double alpha2;
+
+
 	TriangleCatalogEntry(int , int , int , double , double , double );
+	TriangleCatalogEntry();
+
 	void setBefore(TriangleCatalogEntry*);
 	void setNext(TriangleCatalogEntry*);
 	TriangleCatalogEntry* getBefore();
 	TriangleCatalogEntry* getNext();
 };
-
+TriangleCatalogEntry::TriangleCatalogEntry(){
+	this->before=NULL;
+	this->next=NULL;
+}
 
 
 //Eintrag für ein Dreieck, immer das kleinste
@@ -75,6 +97,8 @@ TriangleCatalogEntry::TriangleCatalogEntry(int id1, int id2, int id3, double bet
 	this -> beta=beta; //Winkel im Dreieck, alpah1+alpha2 sei minimal, 180-alpha1-alpha2=beta
 	this -> alpha1=alpha1;
 	this -> alpha2=alpha2;
+	this->before=NULL;
+	this->next=NULL;
 }
 
 void TriangleCatalogEntry::setBefore(TriangleCatalogEntry* before){
@@ -95,8 +119,8 @@ TriangleCatalogEntry* TriangleCatalogEntry::getNext(){
 
 class Catalog{
 public:
-	StarCatalogEntry *head_starlog;
-	TriangleCatalogEntry *head_trilog;
+	StarCatalogEntry *head_starlog = new StarCatalogEntry();
+	TriangleCatalogEntry *head_trilog=new TriangleCatalogEntry();
 	void makeCatalog();
 	TriangleCatalogEntry searchTriangle(StarCatalogEntry* );	
 
@@ -151,44 +175,49 @@ int getNumberOfLines(FILE *fp){
 
 void Catalog::makeCatalog(){
 	
-
+	printf("Start Creating Catalogs");
 	FILE *file;
 	file= fopen("catalog.txt", "r");
 	double size=getNumberOfLines(file);
 	fclose(file);
 	file= fopen("catalog.txt", "r");
-	//StarCatalogEntry *log[] = new StarCatalogEntry[size-1];
-	//this->trilog= TriangelCatalogEntry[size-1]();
 	
 	double azimut,elevation,mag;
 	int index; 
 	StarCatalogEntry  *s_before=this->head_starlog;
+	printf(" vorgänger  (head) %d \n",s_before->id);
 	StarCatalogEntry  *next;	
 	double x,y,z;
 	while(!feof(file)){
 		//**Sternenkatalog wird als Verkettete Liste gespeichert**
 		
-		fscanf(file, "%d %lf %lf %lf\n", &index, &azimut,&elevation,&mag );
+		fscanf(file, "%d %lf %lf %lf\n", &index, &azimut,&elevation,&mag ); 	
 		if (mag<6.5){		
-			
+			printf(" Starcatalog index %d  \n", index);
 			double f=0.25; //Brennweite in Millimeter...vielleicht lieber 10 pc ??, radius eig egal!
 			//**Umrechnung in Kartesische Koordinaten**	
 			x= f*sin(elevation)*cos(azimut);
 			y=f*sin(elevation)*sin(azimut);
 			z=f*cos(elevation);
-			StarCatalogEntry e(s_before,index, x,y,z,mag, azimut, elevation);
-			s_before->next=&e;			
-			s_before=&e;	
+			StarCatalogEntry *e= new StarCatalogEntry(s_before,index, x,y,z,mag, azimut, elevation);
+			//printf(" vorgänger %d \n",e->before->id);
+			s_before->next= e;
+			//printf(" NACHfolger des vorgänger %d \n",e->before->next->id);	
+			//printf("-------------------------------------------------------------\n");	
+			s_before=e;	
 		}
 	}	
 
-	StarCatalogEntry *star=head_starlog->next;
+	StarCatalogEntry *star=this->head_starlog->next;
+	printf("Trianglecatalog index %d\n", star->id);
 	TriangleCatalogEntry *t_before=this->head_trilog;
-	while (star!=NULL){
-		TriangleCatalogEntry t=searchTriangle(star);
-		t.setBefore(t_before);
-		t_before=&t;
-	}
+//	while (star!=NULL){
+//		printf("Trianglecatalog index %d", star->id);
+//		TriangleCatalogEntry t=searchTriangle(star);
+//		t.setBefore(t_before);
+//		t_before=&t;
+//		star=star->next;
+//	}
 	
 }	
 
